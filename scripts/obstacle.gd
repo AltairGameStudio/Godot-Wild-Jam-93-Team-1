38d05b262567b3dc538d053290974fa64e9668a3
Pedro Henrique Coterli <pedrocoterli@gmail.com>
2026-05-17 10:41:33 -0300

    Adicionando som de desbloqueio dos obstáculos

--- scripts/obstacle.gd
+++ scripts/obstacle.gd
@@ -7,7 +7,7 @@ extends StaticBody2D
 @export_multiline var success_text: String = "..."
 @export_multiline var wrong_item_text: String = "This doesn't seem to work here..."
 
-@onready var unlocked_sound = $AudioStreamPlayer2D
+@onready var unlocked_sound = $UnlockingSound
 
 # Interação para dar dicas ao jogador
 func on_interact() -> void:
@@ -16,10 +16,15 @@ func on_interact() -> void:
 func on_item_used(used_item_id: String) -> bool:
 	if used_item_id == required_item:
 		$/root/World/HUD/DialogBox.display_text(success_text, false)
+		
+		unlocked_sound.reparent(get_tree().current_scene)
 		unlocked_sound.play()
-		# Destrói o obstáculo
+		
+		# Programa o som para se autodestruir assim que terminar de tocar
+		unlocked_sound.finished.connect(unlocked_sound.queue_free)
+		
 		queue_free()
-		# Retorna verdadeiro para que o inventário saiba que o item foi gasto
+		
 		return true
 	else:
 		$/root/World/HUD/DialogBox.display_text(wrong_item_text, false)
